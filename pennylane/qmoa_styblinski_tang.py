f7cebd35edda9c38d09f90b7debd0284e293ae76
Edric-Matwiejew <edric.matwiejew@research.uwa.edu.au.com>
2023-08-16 13:08:51 +0800

    formatting

--- pennylane/qmoa_styblinski_tang.py
+++ pennylane/qmoa_styblinski_tang.py
@@ -11,20 +11,22 @@ from unitaries import diagonal_pauli_decompose, phase_shift
 from scipy.sparse import csr_matrix as csr
 
 pauli_terms = {
-        'X':X(0,1),
-        'Y':Y(0,1),
-        'Z':Z(0,1),
-        'I':I(1)
-        }
+    'X': X(0, 1),
+    'Y': Y(0, 1),
+    'Z': Z(0, 1),
+    'I': I(1)
+}
 
-def qmoa_hamiltonians(G, dim, wires, base_mixer = None):
+
+def qmoa_hamiltonians(G, dim, wires, base_mixer=None):
 
     wires = np.array(list(wires))
-    dim_wires = np.split(wires,dim)
+    dim_wires = np.split(wires, dim)
 
     G = nx.to_scipy_sparse_array(G)
 
-    terms = [[I(int(np.log2(G.shape[0]))) for _ in range(dim)] for _ in range(dim)]
+    terms = [[I(int(np.log2(G.shape[0]))) for _ in range(dim)]
+             for _ in range(dim)]
 
     kron_terms = []
 
@@ -34,11 +36,13 @@ def qmoa_hamiltonians(G, dim, wires, base_mixer = None):
 
     hamiltonians = []
     for term in kron_terms:
-        hamiltonians.append(qmoa_pauli_string(term, wires, base_mixer = base_mixer))
+        hamiltonians.append(qmoa_pauli_string(
+            term, wires, base_mixer=base_mixer))
 
     return dim_wires, hamiltonians
 
-def qmoa_pauli_string(matrix, wires, base_mixer = None):
+
+def qmoa_pauli_string(matrix, wires, base_mixer=None):
 
     wires = list(wires)
     n_wires = len(wires)
@@ -73,6 +77,7 @@ def qmoa_pauli_string(matrix, wires, base_mixer = None):
     obs = [reduce(matmul, word) for word in words]
     return qml.Hamiltonian(coeffs, obs)
 
+
 def qmoa_hamiltonian_evolution(ts, dim_wires, Hamiltonians):
     """
     No ancilla qubit.
@@ -85,9 +90,10 @@ def qmoa_hamiltonian_evolution(ts, dim_wires, Hamiltonians):
         for word, coeff in zip(Hamiltonian.ops, Hamiltonian.coeffs):
 
             nonidentity = [pgate for pgate in word.name if pgate != 'Identity']
-            nonidentity_wires = [wire for wire, pgate in zip(word.wires, word.name) if pgate != 'Identity']
+            nonidentity_wires = [wire for wire, pgate in zip(
+                word.wires, word.name) if pgate != 'Identity']
             # rename this
-            if (len(nonidentity_wires) == 0): 
+            if (len(nonidentity_wires) == 0):
                 continue
 
             wires_max = max(nonidentity_wires)
@@ -99,8 +105,7 @@ def qmoa_hamiltonian_evolution(ts, dim_wires, Hamiltonians):
                     qml.RZ(-3*qml.numpy.pi/4, wires=wire)
                     qml.RY(qml.numpy.pi/4, wires=wire)
                     qml.RZ(-3*qml.numpy.pi/4, wires=wire)
-                    
-                
+
             for i, (pgate, wire) in enumerate(zip(nonidentity, nonidentity_wires)):
                 if (wire < wires_max) and (pgate != 'Identity'):
                     qml.CNOT(wires=[wire, nonidentity_wires[i + 1]])
@@ -108,7 +113,7 @@ def qmoa_hamiltonian_evolution(ts, dim_wires, Hamiltonians):
             qml.RZ(2 * t * coeff, wires=wires_max)
 
             for i, (pgate, wire) in enumerate(zip(reversed(nonidentity), reversed(nonidentity_wires))):
- 
+
                 if (wire < wires_max) and (pgate != 'Identity'):
                     qml.CNOT(wires=[wire, nonidentity_wires[-i]])
 
@@ -121,15 +126,22 @@ def qmoa_hamiltonian_evolution(ts, dim_wires, Hamiltonians):
                     qml.RY(-qml.numpy.pi/4, wires=wire)
                     qml.RZ(3*qml.numpy.pi/4, wires=wire)
 
+
 def styblinski_tang(x):
     x = np.array(x)
     return 0.5 * (5 * x - 16 * x**2 + x**4).sum()
 
+
 def styblinski_tang_hamiltonian(dim_wires, qubits_per_dim):
-    grid_points = 2**qubits_per_dim 
-    qualities_1D = diagonal_pauli_decompose([styblinski_tang([-5 + i*10/grid_points]) for i in range(grid_points)])
-    coeffs = [] 
+
+    grid_points = 2**qubits_per_dim
+
+    qualities_1D = diagonal_pauli_decompose(
+        [styblinski_tang([-5 + i*10/grid_points]) for i in range(grid_points)])
+
+    coeffs = []
     obs = []
+
     for dim_wire_set in dim_wires:
         for coeff, op in zip(qualities_1D.coeffs, qualities_1D.ops):
             wtemp = None
@@ -140,10 +152,11 @@ def styblinski_tang_hamiltonian(dim_wires, qubits_per_dim):
                     else:
                         wtemp @= qml.Identity(dim_wire_set[wire])
                 elif name == 'PauliZ':
-                    if wtemp ==  None:
+                    if wtemp == None:
                         wtemp = qml.PauliZ(dim_wire_set[wire])
                     else:
                         wtemp @= qml.PauliZ(dim_wire_set[wire])
             obs.append(wtemp)
             coeffs.append(coeff)
+
     return qml.Hamiltonian(coeffs, obs)
