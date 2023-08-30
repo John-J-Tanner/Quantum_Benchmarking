98b891ca25c27f13144beb7648465fd0c437b203
Edric-Matwiejew <edric.matviev@example.com>
2023-08-30 11:58:02 +0800

    pass integer seed to evolution_benchmark.py

--- pennylane/evolution_benchmarks.sh
+++ pennylane/evolution_benchmarks.sh
@@ -1,10 +1,19 @@
+### INPUT ARGUMENTS 
+
 # Whitespace delimited list of Pennylane backends, e.g. "default.qubit lightning.qubit".
 backends="$1"
 # Directory in which to output benchmark results.
 results_rootdir="$2"
-
+# Subfolder to seperate benchmark results (dates, different configs and so on).
+benchmark_set_ID="$3"
 # Time-limit (seconds) applied to each combination of state-evolution benchmark function + simulation backend.
-benchmark_time_limit=$(( 60 * 5 ))
+benchmark_time_limit=$4
+# Lower and upper bound for the number of simulated qubits.
+qubits_min=$5
+qubits_max=$6
+
+###################
+
 # Number of repeats.
 n_repeats=3
 # Modules containing state evolution benchmark functions.
@@ -15,19 +24,17 @@ ansatze=(qmoa_complete_ST_evolution_QFT qmoa_complete_ST_evolution_HS qaoa_compl
 ansatz_args=(4 4 "" "")
 # Simulated ansatz iterations.
 depths=(1 2 4 8 16 32 64)
-# Lower and upper bound for the number of simulated qubits.
-qubits_min=4
-qubits_max=30
 # Number of sequential expectation value evaluations to carry out with randomly generated variational parameters.
 # The total number of state propagations is depth*n_expval.
 n_expval=100
 
-# The results for a run of the evolution_benchmarks.sh script are saved to a unique subfolder in the results_rootdir.
-
-benchmark_set_ID=$(date +%s)
 output_dir="$results_rootdir"/$benchmark_set_ID
 mkdir -p "$output_dir"
 echo Output directory: "$output_dir"
+
+# Number of GPUS, assumed to be zero if $SLURM_GPUS is not defined.
+N_GPUS=${SLURM_GPUS+0}
+echo N_GPUS: $N_GPUS
 
 for backend in ${backends[@]}; do
 	ansatz_index=0
@@ -47,9 +54,12 @@ for backend in ${backends[@]}; do
 		#	depth: 			simulated circuit depth
 		#	n_expval:  		number of expectation value evaluations
 		#	last_expval:	last expectation value, should be the same for all simulation backends
-		#	func_time:		in-program time (seconds) taken call the state-evolution benchmark function.
+		#	func_time:		in-program time (seconds) taken call the state-evolution benchmark function
 		#	circuit_time:	in-program time (seconds) taken to carry out the state evolutions (excludes setup steps)
 		#	wall_time:		program wall time
+		#	n_nodes:		number of nodes (SLURM_NNODES)
+		#	n_cpus:			number of CPUs per task (SLURM_CPUS_PER_TASK)
+		#	n_gpus:			number of GPUS (SLURM_GPUS)
 		#
 		echo repeat,ansatz,backend,qubits,depth,n_expval,last_expval,func_time,circuit_time,wall_time,n_nodes,n_cpus,n_gpus >$output_name
 		for qubits in $(seq $qubits_min $qubits_max); do
@@ -60,16 +70,11 @@ for backend in ${backends[@]}; do
 					echo "(Benchmark time remaining: $time_remaining%)": Running repeat $repeat with $backend backend for $ansatz evolution with $qubits qubits at depth $depth.
 
 					start=$(date +%s.%N)
-					case $SLURM_GPUS in
-						1)	
-						output=$(srun -N $SLURM_NNODES -n $SLURM_NTASKS -c $SLURM_CPUS_PER_TASK --gpus=$SLURM_GPUS python evolution_benchmark.py $backend ${ansatz_modules[$ansatz_index]} $ansatz $qubits $depth $n_expval ${ansatz_args[$ansatz_index]})
-						N_GPUS=$SLURM_GPUS
-						;;
-						*)
-						output=$(srun -N $SLURM_NNODES -n $SLURM_NTASKS -c $SLURM_CPUS_PER_TASK python evolution_benchmark.py $backend ${ansatz_modules[$ansatz_index]} $ansatz $qubits $depth $n_expval ${ansatz_args[$ansatz_index]})
-						N_GPUS=1
-						;;
-					esac
+					if [ $N_GPUS -gt 0 ]; then
+						output=$(srun -N $SLURM_NNODES -n $SLURM_NTASKS -c $SLURM_CPUS_PER_TASK --gpus=$SLURM_GPUS python evolution_benchmark.py $backend ${ansatz_modules[$ansatz_index]} $ansatz $qubits $depth $n_expval $repeat ${ansatz_args[$ansatz_index]}) 
+					else
+						output=$(srun -N $SLURM_NNODES -n $SLURM_NTASKS -c $SLURM_CPUS_PER_TASK python evolution_benchmark.py $backend ${ansatz_modules[$ansatz_index]} $ansatz $qubits $depth $n_expval $repeat ${ansatz_args[$ansatz_index]})
+					fi
 					end=$(date +%s.%N)
 
 					status=$(echo "$output" | cut -d, -f 1)	
