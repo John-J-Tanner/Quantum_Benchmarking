6e3f776cc0f6b5f4ded5e526833d204a38c781ad
Edric-Matwiejew <edric.matwiejew@research.uwa.edu.au>
2023-08-31 21:08:03 +0800

    Alternative Grovers search circuit with the MulticontrolledX decomposed using one extra qubit, required for backends with a limited gate set

--- pennylane/grovers_search.py
+++ pennylane/grovers_search.py
@@ -1,8 +1,34 @@
 from time import time
 import pennylane as qml
 import pennylane.numpy as np
-from unitaries import bin_array
+from unitaries import bin_array, diagonal_pauli_decompose, phase_shift, circulant_mixer, complete_eigenvalues
+from scipy.sparse import csr_array
+ 
+def _decomposition_with_one_worker(control_wires, target_wire, work_wire):
+    """Decomposes the multi-controlled PauliX gate using the approach in Lemma 7.3 of
+    https://arxiv.org/abs/quant-ph/9503016, which requires a single work wire"""
+    tot_wires = len(control_wires) + 2
+    partition = int(np.ceil(tot_wires / 2))
 
+    first_part = control_wires[:partition]
+    second_part = control_wires[partition:]
+
+    qml.MultiControlledX(
+        wires=first_part + work_wire,
+        work_wires=second_part + target_wire,
+    )
+    qml.MultiControlledX(
+        wires=second_part + work_wire + target_wire,
+        work_wires=first_part,
+    )
+    qml.MultiControlledX(
+        wires=first_part + work_wire,
+        work_wires=second_part + target_wire,
+    )
+    qml.MultiControlledX(
+        wires=second_part + work_wire + target_wire,
+        work_wires=first_part,
+    )
 
 def grovers_search(device, depth, n_expvals, seed, *args):
     qubits = len(device.wires)
@@ -28,39 +54,39 @@ def grovers_search(device, depth, n_expvals, seed, *args):
     return float(np.mean(expval)), time() - start
 
 def grovers_search_decomposed(device, depth, n_expvals, seed, *args):
-    qubits = len(device.wires)
-    search_qubits=qubits - 1
-    search_wires = [i for i in range(search_qubits)]
-    workwires=[search_qubits]
+    qubits = len(device.wires) - 1
+    wires = [i for i in range(qubits)]
+    work_wires=[qubits]
 
     @qml.qnode(device)
-    def circuit(marked_state, depth):
-        for wire in search_wires:
+    def circuit(H, depth):
+        for wire in wires:
             qml.Hadamard(wire)
         for _ in range(depth):
-            qml.FlipSign.compute_decomposition(arr_bin=marked_state, wires=search_wires)
-            qml.GroverOperator.compute_decomposition(wires=search_wires, work_wires=workwires)
-
-        return qml.probs(wires=search_wires)
+            phase_shift(np.pi, wires, H)
+            for wire in wires[:-1]:
+                qml.Hadamard(wire)
+                qml.PauliX(wire)
+            qml.PauliZ(wires[-1])
+            _decomposition_with_one_worker(wires[:-1], [wires[-1]], work_wires)
+            qml.PauliZ(wires[-1])
+            for wire in wires[:-1]:
+                qml.PauliX(wire)
+                qml.Hadamard(wire)
+        return qml.probs(wires=wires)
 
     start = time()
     rng = np.random.default_rng(seed)
     for _ in range(n_expvals):
-        marked_int = rng.integers(0, 2**search_qubits)
-        #drawer = qml.draw(circuit)
-        marked_state = bin_array(marked_int, search_qubits)
-        expval = circuit(marked_state, depth)[marked_int]
-        #print(drawer(marked_state,depth))
-        
-
+        marked_int = rng.integers(0, 2**qubits)
+        H = diagonal_pauli_decompose(csr_array(([1], ([marked_int], [0])), shape = (2**qubits, 1)))
+        expval = circuit(H, depth)[marked_int]
     return float(np.mean(expval)), time() - start
 
-
-
 if __name__ == "__main__":
-    for qubits in range(2, 8):
-        device = qml.device("default.qubit", wires=qubits + 1)
+    for qubits in range(3, 8):
+        device = qml.device("lightning.qubit", wires=qubits + 1)
         depth = int(np.sqrt(2**qubits) * np.pi / 4)
         print(qubits, depth)
         for repeat in [1, 2, 3]:
-            print(grovers_search_decomposed(device, depth, 1, repeat))
+            print(grovers_search_decomposed(device, depth, 3, repeat))
