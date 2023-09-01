4b41b37b3262dd13ead4ec48ce04ae1581a7ce1a
Edric-Matwiejew <edric.matviev@example.com>
2023-09-01 12:32:31 +0800

    scripts to submit jobs

---
+++ pennylane/slurm_scripts/lightning_qubit/launch_jobs.sh
@@ -0,0 +1,22 @@
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+. $SCRIPT_DIR/../../defaults.sh
+
+cpus=(1 16 32 64)
+
+for cpus_per_task in ${cpus[@]}; do
+	for repeat in $(seq 1 3); do
+		sbatch --output=$BENCHMARK_OUTPUT/${repeat}_1_1_${cpus_per_task}_0_qmoa_qft_lightning_qubit.out --mem=$((2000 * $cpus_per_task)) --cpus-per-task=$cpus_per_task $SCRIPT_DIR/qmoa_qft.slurm $repeat
+		sbatch --output=$BENCHMARK_OUTPUT/${repeat}_1_1_${cpus_per_task}_0_qmoa_hs_lightning_qubit.out --mem=$((2000 * $cpus_per_task)) --cpus-per-task=$cpus_per_task $SCRIPT_DIR/qmoa_hs.slurm $repeat
+		sbatch --output=$BENCHMARK_OUTPUT/${repeat}_1_1_${cpus_per_task}_0_qaoa_lightning_qubit.out --mem=$((2000 * $cpus_per_task)) --cpus-per-task=$cpus_per_task $SCRIPT_DIR/qaoa.slurm $repeat
+		sbatch --output=$BENCHMARK_OUTPUT/${repeat}_1_1_${cpus_per_task}_0_grovers_lightning_qubit.out --mem=$((2000 * $cpus_per_task)) --cpus-per-task=$cpus_per_task $SCRIPT_DIR/grovers.slurm $repeat
+	done
+done
+
+cpus_per_task=128
+for repeat in $(seq 1 3); do
+	sbatch --exclusive --output=$BENCHMARK_OUTPUT/${repeat}_1_1_${cpus_per_task}_0_qmoa_qft_lightning_qubit.out --cpus-per-task=$cpus_per_task $SCRIPT_DIR/qmoa_qft.slurm $repeat
+	sbatch --exclusive --output=$BENCHMARK_OUTPUT/${repeat}_1_1_${cpus_per_task}_0_qmoa_hs_lightning_qubit.out --cpus-per-task=$cpus_per_task $SCRIPT_DIR/qmoa_hs.slurm $repeat
+	sbatch --exclusive --output=$BENCHMARK_OUTPUT/${repeat}_1_1_${cpus_per_task}_0_qaoa_lightning_qubit.out --cpus-per-task=$cpus_per_task $SCRIPT_DIR/qaoa.slurm $repeat
+	sbatch --exclusive --output=$BENCHMARK_OUTPUT/${repeat}_1_1_${cpus_per_task}_0_grovers_lightning_qubit.out --cpus-per-task=$cpus_per_task $SCRIPT_DIR/grovers.slurm $repeat
+done
+
