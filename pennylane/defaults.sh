2e2c2027689690030511d8619f42a30619625de3
Edric-Matwiejew <edric.matviev@example.com>
2023-09-01 12:30:14 +0800

    default environment variables

---
+++ pennylane/defaults.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export PENNYLANE_ROOT=$(dirname "$(readlink -f -- "$0")")
+export BENCHMARK_ROOT=$(pwd)
+export BENCHMARK_OUTPUT=$BENCHMARK_ROOT/output
+export PYTHON_VENV_DIR=$BENCHMARK_ROOT/venvs
+export CPU_VENV=$PYTHON_VENV_DIR/cpu
+export GPU_VENV=$PYTHON_VENV_DIR/gpu
+export BENCHMARK_RUN_ID=01-09-2023
+export BENCHMARK_DEFAULTS=set
+
+mkdir -p $PYTHON_VENV_DIR
+mkdir -p $BENCHMARK_OUTPUT
